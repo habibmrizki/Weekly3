c60924d8d58a5cdb6ea6828e85de0d9801c4dc8f
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-07-25 06:22:23 +0700

    feat: adding new code about localstorage

--- assets/js/toggleHamburgeMenu.js
+++ assets/js/toggleHamburgeMenu.js
@@ -1,11 +1,38 @@
 function toggleHamburgerMenu() {
   const navbarNav = document.querySelector("#navbarNav");
-  const navbatBtn = document.querySelector("#navbar-button");
+  const navbarBtn = document.querySelector("#navbar-button");
+  const hamburger = document.querySelector("#hamburger-menu");
 
-  document.querySelector("#hamburger-menu").onclick = () => {
+  hamburger.addEventListener("click", () => {
     navbarNav.classList.toggle("active");
-    navbatBtn.classList.toggle("active");
-  };
+    navbarBtn.classList.toggle("active");
+  });
 }
 
-toggleHamburgerMenu();
+function renderNavbarUserState() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const authButtons = document.querySelector(".auth-buttons");
+  const userProfile = document.querySelector(".user-profile");
+  const userAvatar = document.getElementById("userAvatar");
+
+  if (user) {
+    authButtons.style.display = "none";
+    userProfile.style.display = "inline-block";
+
+    userAvatar.src = user.avatar || "assets/img/navbar-profile.svg";
+  } else {
+    authButtons.style.display = "flex";
+    userProfile.style.display = "none";
+  }
+}
+
+function logout() {
+  localStorage.removeItem("user");
+  alert("Logout berhasil!");
+  window.location.href = "sign-in.html";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  toggleHamburgerMenu();
+  renderNavbarUserState();
+});
